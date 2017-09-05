0ce71f95fac65d73bdc13a9ed5ac05819c8df2b4
jaimehrubiks <jaimehidalgo2@hotmail.com>
2017-09-05 11:04:53 +0200

    autenticacion

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
+var basicAuth = require('express-basic-auth');
+
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+    extended: true
 }));
 
 app.use(express.static('public'));
 
 app.post('/test/', function(req, res){
-    console.log(req.body.data);
     io.emit('msg', req.body.data);
     res.status(200).end();
 });
+
+app.use(basicAuth({
+    challenge: true,
+    users: { 'admin': 'bd1234' }
+}))
+
+app.get('/admin/', function(req,res){
+    res.sendFile('admin.html', {root: './private'})
+})
 
 io.on('connection', function(socket){
     console.log('user connected');
